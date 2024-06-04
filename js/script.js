5112cd17045dd9bdafb4aa86c47140994880d728
stiansame <stianrostad@gmail.com>
2024-06-04 22:07:00 +0200

    Accordion rewritten
    Rewritten all Accordions using Javascript

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ export const favouritesContainer = document.querySelector(".favourites");
 export const message = createMessage();
 
 initialize();
+
+//Accordion
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
